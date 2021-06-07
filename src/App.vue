414a1a65c1c919e06c5116103f3c2b7d7e80b4c6
dfgiordano <federico.giordano90@gmail.com>
2021-06-07 15:20:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Title />
+    <Heroes />
+  </div>
+</template>
+
+<script>
+import Title from './components/Title';
+import Heroes from './components/Heroes';
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    Heroes
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Heroes.vue
@@ -0,0 +1,13 @@
+<template>
+    <img src="../assets/supereroi.jpeg" alt="Heroes">
+</template>
+
+<script>
+export default {
+    name : "Heroes"
+}
+</script>
+
+<style>
+
+</style>
